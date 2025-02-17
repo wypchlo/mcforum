97fb1063d9b7cb5dcae35a89e19ea9287025166e
wypchlo <deron.pl.yt@gmail.com>
2025-02-17 21:02:33 +0100

    Remvoed accidentally commited target dir

--- api/target/debug/deps/api-ae3f2e3a54fd5328.d
+++
@@ -1,5 +0,0 @@
-/home/deron/Documents/projects/mcforum/api/target/debug/deps/libapi-ae3f2e3a54fd5328.rmeta: src/main.rs
-
-/home/deron/Documents/projects/mcforum/api/target/debug/deps/api-ae3f2e3a54fd5328.d: src/main.rs
-
-src/main.rs:
--- api/target/debug/deps/api-de41a41696ba0bb2.d
+++
@@ -1,5 +0,0 @@
-/home/deron/Documents/projects/mcforum/api/target/debug/deps/libapi-de41a41696ba0bb2.rmeta: src/main.rs
-
-/home/deron/Documents/projects/mcforum/api/target/debug/deps/api-de41a41696ba0bb2.d: src/main.rs
-
-src/main.rs:
